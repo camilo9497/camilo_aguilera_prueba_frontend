0f5464d518346008fbac03bd5c1810cebb441e31
Camilo Aguilera <camilo9497@live.com>
2024-03-06 16:07:37 -0500

    add animation

--- src/components/AnimationSection/index.tsx
+++ src/components/AnimationSection/index.tsx
@@ -4,7 +4,8 @@ const AnimationSection = () => {
   return (
     <div className={styles.container}>
       <div className={styles.containerSecondary}>
-      <h1>Animacion</h1>
+        <h1 className={styles.title}>Pellentesque habitant morbi.</h1>
+        <p className={styles.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore exercitationem soluta totam! Illum, dolore nisi consectetur ipsum eius voluptatem facilis iste neque nam rem deleniti architecto adipisci ab tempore officiis!</p>
       </div>
    
     </div>
