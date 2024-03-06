40114a391ae1a629aedfb2f6544e750e357997d7
Camilo Aguilera <camilo9497@live.com>
2024-03-06 12:01:05 -0500

    add footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,18 @@
+
+import ItemFooter from '../ItemFooter';
+import styles from './styles.module.css'
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.containerSecond}>
+        <ItemFooter title='Lorem ipsum dolor amet' description='Quisque sagittis hendrerit justo, eu aliquam risus posuere sed' />
+        <ItemFooter title='Lorem ipsum dolor amet' description='Quisque sagittis hendrerit justo, eu aliquam risus posuere sed' />
+        <ItemFooter title='Lorem ipsum dolor amet' description='Quisque sagittis hendrerit justo, eu aliquam risus posuere sed' />
+      </div>
+      <p className={styles.text}>Lorem ipsum dolor sit ame <p onClick={()=>console.log('footer link')} className={styles.textLink}>facere facilisxxx</p>  eaque aliquid ut molestias libero iste corrupti sapiente incidunt obcaecati?</p>
+    </div>
+  )
+}
+
+export default Footer;
