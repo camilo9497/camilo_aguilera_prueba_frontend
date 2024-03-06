bb76c270cbe085bedcbecb0fb21322ac96af5250
Camilo Aguilera <camilo9497@live.com>
2024-03-05 19:01:04 -0500

    add onpress prop

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,11 +3,12 @@ import styles from './styles.module.css'
 interface Props {
   text: string
   icon?: boolean
+  onPress?: any
 }
 
-const Button: React.FC<Props> = ({text, icon = false}) => {
+const Button: React.FC<Props> = ({text, icon = false, onPress}) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={()=>onPress()}  >
       <p className={`${styles.text}`}>{text}</p>
       {icon && <div className={styles.containerIcon}>X</div>}
     </div>
