e2c9ac3e02ca43b23e04fb476479165ae08cdda2
Camilo Aguilera <camilo9497@live.com>
2024-03-05 19:02:37 -0500

    add hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,13 +3,13 @@ import styles from './styles.module.css'
 
 const Hero = () => {
   return (
-    <>
-      <h1>Lorem ipsum</h1>
-      <h2>Sed ullamcorper odio ullamcorper</h2>
-      <p>"Duis bibendum, lacus nec ullamcorper dapibus, tortor orci aliquam ante..."</p>
-      <p>"Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus...."</p>
-      <Button text='Go there...' icon={true}/>
-    </>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Lorem ipsum</h1>
+      <h2 className={styles.subtitle}>Sed ullamcorper odio ullamcorper</h2>
+      <p className={styles.description}>"Duis bibendum, lacus nec ullamcorper dapibus, tortor orci aliquam ante..."</p>
+      <p className={styles.description}>"Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus...."</p>
+      <Button text='Go there...' icon={true} onPress={()=>console.log('Go there...')} />
+    </div>
   )
 }
 
